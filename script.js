134146e85e35f6f819f05b421f9ab85d83582468
unknown <fl.kawsarahmed@gmail.com>
2022-07-27 00:09:53 +0600

    learning JSX

--- script.js
+++ script.js
@@ -8,39 +8,58 @@
 // ReactDOM.render(elms, document.querySelector(".container"));
 
 //React component
+// function bio(props) {
+//   //   console.log(props);
+//   return React.createElement(
+//     "div",
+//     { id: "profile" },
+//     React.createElement(
+//       "h1",
+//       { className: "Clsname" },
+//       `i am ${props.name} ahmed`
+//     ),
+//     React.createElement("p", { id: "pg" }, `i am a ${props.prof}`)
+//   );
+// }
+
+// function getProfession() {
+//   React.createElement(
+//     "div",
+//     { id: "divId" },
+//     React.createElement(bio, { name: "kawsar", prof: "js developer" })
+//   ),
+//     React.createElement(
+//       "div",
+//       { id: "divId" },
+//       React.createElement(bio, { name: "kamal", prof: "php developer" })
+//     ),
+//     React.createElement(
+//       "div",
+//       { id: "divId" },
+//       React.createElement(bio, { name: "kamal", prof: "React developer" })
+//     );
+// }
+
+// ReactDOM.render(
+//   React.createElement(getProfession),
+//   document.querySelector(".container")
+// );
+
+//jsx start
 function bio(props) {
-  //   console.log(props);
-  return React.createElement(
-    "div",
-    { id: "profile" },
-    React.createElement(
-      "h1",
-      { className: "Clsname" },
-      `i am ${props.name} ahmed`
-    ),
-    React.createElement("p", { id: "pg" }, `i am a ${props.prof}`)
+  //jsx
+  <div className="profile">
+    <h1>i am {props.name}</h1>
+    <p>i am a {props.prof}</p>
+  </div>
+
+function getProfile() {
+  return (
+    <div className="bioata">
+      <bio name="kawsar" prof="webdeveloper" />;
+      <bio name="khalil" prof="designer" />;
+    </div>
   );
 }
 
-function getProfession() {
-  React.createElement(
-    "div",
-    { id: "divId" },
-    React.createElement(bio, { name: "kamal", prof: "js developer" })
-  ),
-    React.createElement(
-      "div",
-      { id: "divId" },
-      React.createElement(bio, { name: "kamal", prof: "js developer" })
-    ),
-    React.createElement(
-      "div",
-      { id: "divId" },
-      React.createElement(bio, { name: "kamal", prof: "js developer" })
-    );
-}
-
-ReactDOM.render(
-  React.createElement(getProfession),
-  document.querySelector(".container")
-);
+ReactDOM.render(<getProfile />, document.querySelector(".container"));
